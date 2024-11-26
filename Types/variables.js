3b4d48712f6fca5296be8ab9543216dcf77d8a57
narender <narendarmekala.9778@gmail.com>
2024-11-26 16:18:54 +0530

    Data Types in TS

---
+++ Types/variables.js
@@ -0,0 +1,7 @@
+var movieTitle = "Pushpa";
+movieTitle = "pushpa2";
+movieTitle.toUpperCase();
+var myNumber = 42;
+myNumber = 60;
+var myBoolean = true;
+console.log(movieTitle);
---
+++ Types/variables.ts
@@ -0,0 +1,11 @@
+let movieTitle: string = "Pushpa";
+movieTitle = "pushpa2";
+movieTitle.toUpperCase();
+
+
+let myNumber: number = 42;
+myNumber =60;
+
+const myBoolean: boolean = true;
+
+console.log(movieTitle);
