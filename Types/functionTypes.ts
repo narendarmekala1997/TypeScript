36503716c1f61929645b22b5dc80c1388d6cd2a7
narender <narendarmekala.9778@gmail.com>
2024-11-27 16:38:55 +0530

    Default Types

--- Types/functionTypes.ts
+++ Types/functionTypes.ts
@@ -14,3 +14,12 @@ function greet(person: string){
     return 'Hi there,${person}';
 }
 greet("naren");
+
+//default value to function parameters
+function square1(num: number=10 ){
+    return num*num;
+}
+
+function greetingWithDefault( person: string = "Stranger"){
+    return 'Hi there, ${person}!';
+}
